91594a3a0cce6691d1657eb34ce77be64b7f130f
inlineflow <thrashdev@protonmail.com>
2025-05-15 17:26:50 +0600

    added shit

--- src/UserProfile.tsx
+++ src/UserProfile.tsx
@@ -9,12 +9,12 @@ export default function UserProfile() {
       <div className="flex ">
         <Inputbox
           label="First Name"
-          className="h-1 mb-12"
+          className="h-6 mb-12 border-2"
           labelClass=" text-base"
         />
         <Inputbox
           label="Last Name"
-          className="h-1 mb-12"
+          className="h-6 mb-12 border-2"
           labelClass=" text-base"
         />
       </div>
