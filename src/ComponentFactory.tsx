e62c914223c4767feab4f8be90c3bba924b2756e
inlineflow <thrashdev@protonmail.com>
2025-05-29 15:16:45 +0600

    added shit?

--- src/ComponentFactory.tsx
+++ src/ComponentFactory.tsx
@@ -1,10 +1,10 @@
 import type { JSX } from "react";
-import type { UUID } from "./util/uuid";
+import { generateUUID, type UUID } from "./util/uuid";
 
 type ComponentFactory = <P extends object>(
   Component: React.ComponentType<P>,
   props?: P
-) => (id: UUID) => JSX.Element;
+) => ({ id }: { id?: UUID }) => JSX.Element;
 
 // export const cf: ComponentFactory = (Component, props?) => () =>
 //   <Component {...(props ?? ({} as P))} />;
@@ -12,6 +12,10 @@ type ComponentFactory = <P extends object>(
 export const cf: ComponentFactory = <P extends object>(
   Component: React.ComponentType<P>,
   props?: P
-): ((id: UUID) => JSX.Element) => {
-  return (id: UUID) => <Component {...(props ?? ({} as P))} />;
+): (({ id }: { id?: UUID }) => JSX.Element) => {
+  return ({ id }: { id?: UUID }) => {
+    id = id ?? generateUUID();
+    const newProps = { ...(props ?? ({} as P)), id: id };
+    return <Component {...newProps} />;
+  };
 };
