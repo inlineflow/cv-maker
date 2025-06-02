a4536e8e76e3ba4b19be8789c55e5c738962b7d0
inlineflow <thrashdev@protonmail.com>
2025-06-02 16:49:57 +0600

    added shit

--- src/InputDateRange.tsx
+++ src/InputDateRange.tsx
@@ -1,14 +1,18 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 import moment from "moment";
 
 type InputDateProps = {
   htmlID: string;
 };
 
+const dateFormat = "YYYY-MM-DD"; // ISO-8601
+
 export const InputDateRange = ({ htmlID }: InputDateProps) => {
-  const startDateRef = useRef<HTMLInputElement | null>(null);
-  const endDateRef = useRef<HTMLInputElement | null>(null);
-  const [startDate, setStartDate] = useState(new Date());
+  const [displayStartDate, setDisplayStartDate] = useState("");
+  const [displayEndDate, setDisplayEndDate] = useState("");
+  const startDate = moment(displayStartDate, dateFormat);
+  const endDate = moment(displayEndDate, dateFormat);
+  const dateDiff = endDate.diff(startDate, "days");
 
   return (
     <div className="date-range">
@@ -19,22 +23,27 @@ export const InputDateRange = ({ htmlID }: InputDateProps) => {
             type="date"
             name="start-date"
             id="start-date"
-            ref={startDateRef}
-            value={startDate.toDateString()}
-            onChange={(e) => {
-              const x = new Date(e.target.value);
-              setStartDate(x);
-              console.log(x);
+            value={displayStartDate}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              setDisplayStartDate(e.target.value);
             }}
           />
         </div>
         <span> — </span>
         <div className="end-date-container">
           <label htmlFor="end-date"></label>
-          <input type="date" name="end-date" id="end-date" ref={endDateRef} />
+          <input
+            type="date"
+            name="end-date"
+            id="end-date"
+            value={displayEndDate}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              setDisplayEndDate(e.target.value);
+            }}
+          />
         </div>
       </div>
-      <p className="text-sm opacity-75">{}</p>
+      <p className="text-sm opacity-75">{dateDiff}</p>
     </div>
   );
 };
